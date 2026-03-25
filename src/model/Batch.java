352a4317f680b28b10cc72f432355a6fff787d6c
stumbleguyz <tanishkashukla16@gmail.com>
2026-03-25 19:35:56 +0530

    Added initial files to make folders visible

---
+++ src/model/Batch.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Batch {
+    private String batchId;
+    private LocalDate expiryDate;
+    private int quantity;
+}
---
+++ src/model/Medicine.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Medicine {
+    private String name;
+    private String category;
+    private double price;
+    private int quantity;
+}
