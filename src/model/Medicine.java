c02bb61c72ffb69b14a2d4b20b4f0fd2675b12db
stumbleguyz <tanishkashukla16@gmail.com>
2026-03-25 19:48:58 +0530

    Added constructors to Medicine and Batch

--- src/model/Medicine.java
+++ src/model/Medicine.java
@@ -5,4 +5,11 @@ public class Medicine {
     private String category;
     private double price;
     private int quantity;
+
+    public Medicine(String name, String category, double price, int quantity) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
