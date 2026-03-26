9104e40e2965646a71889d550aad587fd5707ceb
stumbleguyz <tanishkashukla16@gmail.com>
2026-03-26 08:11:05 +0530

    Implemented basic inventory and order services

--- src/service/InventoryService.java
+++ src/service/InventoryService.java
@@ -1,4 +1,25 @@
 package service;
 
+import java.util.ArrayList;
+import model.Medicine;
+
 public class InventoryService {
+
+    private ArrayList<Medicine> medicines = new ArrayList<>();
+
+    // Add medicine
+    public void addMedicine(Medicine medicine) {
+        medicines.add(medicine);
+        System.out.println("Medicine added successfully.");
+    }
+
+    // View all medicines
+    public void viewMedicines() {
+        for (Medicine m : medicines) {
+            System.out.println("Name: " + m.getName() +
+                    ", Category: " + m.getCategory() +
+                    ", Price: " + m.getPrice() +
+                    ", Quantity: " + m.getQuantity());
+        }
+    }
 }
