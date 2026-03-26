133fa88a5d54beac4f32b706cbeff034332b82a8
SohaPatel <sohapatel1706@gmail.com>
2026-03-26 08:12:48 +0530

    Implemented basic inventory and order services

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,4 +1,24 @@
 package service;
 
+import model.Order;
+import java.util.ArrayList;
+
 public class OrderService {
+
+    private ArrayList<Order> orders = new ArrayList<>();
+
+    // Place order
+    public void placeOrder(Order order) {
+        orders.add(order);
+        System.out.println("Order placed successfully.");
+    }
+
+    // View orders
+    public void viewOrders() {
+        for (Order o : orders) {
+            System.out.println("Order ID: " + o.getOrderId() +
+                    ", Medicine: " + o.getMedicineName() +
+                    ", Quantity: " + o.getQuantity());
+        }
+    }
 }
