47cadfada4af8c587d7c164b1c41fb0199e95499
stumbleguyz <tanishkashukla16@gmail.com>
2026-03-30 10:45:42 +0530

    Added low stock alert feature

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,19 +1,17 @@
 package service;
 
-import model.Order;
 import java.util.ArrayList;
+import model.Order;
 
 public class OrderService {
 
     private ArrayList<Order> orders = new ArrayList<>();
 
-    // Place order
     public void placeOrder(Order order) {
         orders.add(order);
         System.out.println("Order placed successfully.");
     }
 
-    // View orders
     public void viewOrders() {
         for (Order o : orders) {
             System.out.println("Order ID: " + o.getOrderId() +
