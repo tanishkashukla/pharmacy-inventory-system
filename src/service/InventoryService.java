3fe3aa51b01a84a2430ae635e25edc22dbdd4581
stumbleguyz <tanishkashukla16@gmail.com>
2026-03-30 10:28:25 +0530

    Updated Menu with search medicine feature

--- src/service/InventoryService.java
+++ src/service/InventoryService.java
@@ -13,13 +13,32 @@ public class InventoryService {
         System.out.println("Medicine added successfully.");
     }
 
-    // View all medicines
+    // View medicines
     public void viewMedicines() {
         for (Medicine m : medicines) {
             System.out.println("Name: " + m.getName() +
                     ", Category: " + m.getCategory() +
                     ", Price: " + m.getPrice() +
                     ", Quantity: " + m.getQuantity());
+        }
+    }
+
+    // Search medicine
+    public void searchMedicine(String name) {
+        boolean found = false;
+
+        for (Medicine m : medicines) {
+            if (m.getName().equalsIgnoreCase(name)) {
+                System.out.println("Found: " + m.getName() +
+                        ", Category: " + m.getCategory() +
+                        ", Price: " + m.getPrice() +
+                        ", Quantity: " + m.getQuantity());
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Medicine not found.");
         }
     }
 }
